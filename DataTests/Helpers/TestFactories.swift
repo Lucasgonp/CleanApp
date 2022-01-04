65db6165ff174d0cc41deb6471b7f9a9929f671a
Lucas Pereira <lucasgonp@gmail.com>
2022-01-04 11:33:12 -0300

    ensure AlamofireAdapter calls Alamofire with correct Data

--- DataTests/Helpers/TestFactories.swift
+++ DataTests/Helpers/TestFactories.swift
@@ -4,6 +4,10 @@ func makeInvalidData() -> Data {
     return Data("Invalid Data".utf8)
 }
 
+func makeValidData() -> Data {
+    return Data("{\"name\":\"Lucas\"}".utf8)
+}
+
 func makeUrl() -> URL {
     return URL(string: "hrrp://any-url.com")!
 }
