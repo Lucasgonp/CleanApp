895fb7bb32790419e4f0e2b23580706e76f6622a
Lucas Pereira <lucasgonp@gmail.com>
2022-01-05 17:10:48 -0300

    move SUT to a Helper method

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -5,21 +5,23 @@ import UIKit
 
 class SignUpViewControllerTests: XCTestCase {
     func test_loading_is_hidden_on_start() throws {
-        let sd = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sd.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
-        sut.loadViewIfNeeded()
-        XCTAssertEqual(sut.actitityIndicator?.isAnimating, false)
+        XCTAssertEqual(makeSut().actitityIndicator?.isAnimating, false)
     }
     
     func test_sut_implements_loadingView() throws {
-        let sd = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
-        let sut = sd.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
-        XCTAssertNotNil(sut as LoadingView)
+        XCTAssertNotNil(makeSut() as LoadingView)
     }
     
     func test_sut_implements_alertView() throws {
+        XCTAssertNotNil(makeSut() as AlertView)
+    }
+}
+
+extension SignUpViewControllerTests {
+    func makeSut() -> SignUpViewController {
         let sd = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sd.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
-        XCTAssertNotNil(sut as AlertView)
+        sut.loadViewIfNeeded()
+        return sut
     }
 }
