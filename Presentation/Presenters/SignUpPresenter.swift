764e71f5d103a58d63fffea1918b80cdf46b052b
Lucas Pereira <lucasgonp@gmail.com>
2022-01-05 15:23:21 -0300

    ensure SignUpPresenter hide loading after AddAccount completes

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -34,6 +34,7 @@ public final class SignUpPresenter {
                 case .success:
                     break
                 }
+                self.loadingView.display(viewModel: LoadingViewModel(isLoading: false))
             }
         }
     }
