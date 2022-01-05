a6c7a8f43727556e3d5839dca9177581c022f90a
Lucas Pereira <lucasgonp@gmail.com>
2022-01-05 17:32:00 -0300

    ensure SignUpController calls SignUp when saveButton is Tapped

--- UI/SignUpViewController.swift
+++ UI/SignUpViewController.swift
@@ -4,10 +4,23 @@ import UIKit
 
 final class SignUpViewController: UIViewController {
     @IBOutlet weak var actitityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    var signUp: ((SignUpViewModel) -> Void)?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configure()
+    }
+    
+    func configure() {
+        saveButton.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc
+    private func saveButtonTapped() {
+        let model = SignUpViewModel(name: nil, email: nil, password: nil, passwordConfirmation: nil)
+        signUp?(model)
     }
 }
 
