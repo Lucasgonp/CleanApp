9efe089097d25da498e5412631ab19da7d81e29b
Lucas Pereira <lucasgonp@gmail.com>
2022-01-09 19:34:35 -0300

    ensure EmailValidatorAdapter returns true if a valid email is provided

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import Presentation
 
 public final class EmailValidatorAdapter: EmailValidator {
-    private let pattern = "[A-20-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    private let pattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
     
     public func isValid(email: String) -> Bool {
         let range = NSRange(location: 0, length: email.utf16.count)
@@ -19,5 +19,13 @@ class EmailValidatorAdapterTests: XCTestCase {
         XCTAssertFalse(sut.isValid(email: "lu@"))
         XCTAssertFalse(sut.isValid(email: "lu@m"))
         XCTAssertFalse(sut.isValid(email: "@ma.com"))
+    }
+    
+    func test_valid_emails() throws {
+        let sut = EmailValidatorAdapter()
+        XCTAssertTrue(sut.isValid(email: "lucas@mail.com"))
+        XCTAssertTrue(sut.isValid(email: "lucas@cmail.com"))
+        XCTAssertTrue(sut.isValid(email: "lucas@hotmail.com.br"))
+        XCTAssertTrue(sut.isValid(email: "lucas@yahoo.com"))
     }
 }
