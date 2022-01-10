9f05af035fa12384a8fc55e59ee90318cc20573f
Lucas Pereira <lucasgonp@gmail.com>
2022-01-09 22:24:36 -0300

    move ControllerFactory

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -1,11 +1,15 @@
-import Foundation
-import UIKit
 import Domain
+import Foundation
+import Presentation
 import UI
+import Validation
 
 public final class SignUpComposer {
     public static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController {
-        let viewController = ControllerFactory.makeSignUp(addAccount: addAccount)
-        return viewController
+        let controller = SignUpViewController.instantiate()
+        let emailValidatorAdapter = EmailValidatorAdapter()
+        let presenter = SignUpPresenter(alertView: WeakVarProxy(controller), emailValidator: emailValidatorAdapter, addAccount: addAccount, loadingView: WeakVarProxy(controller))
+        controller.signUp = presenter.signUp
+        return controller
     }
 }
