2f2eaf7af9d073966cb2438d8c96424408e708c2
Lucas Pereira <lucasgonp@gmail.com>
2022-01-09 22:17:00 -0300

    ensure SignUpPresenter has no memory leak with SignUpViewController

--- Main/Composers/SignUpComposer.swift
+++ Main/Composers/SignUpComposer.swift
@@ -4,7 +4,7 @@ import Domain
 import UI
 
 public final class SignUpComposer {
-    static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController {
+    public static func composeControllerWith(addAccount: AddAccount) -> SignUpViewController {
         let viewController = ControllerFactory.makeSignUp(addAccount: addAccount)
         return viewController
     }
