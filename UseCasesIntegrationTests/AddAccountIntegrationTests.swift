1370cc23ff50d848144ff78a99d6870ff6960318
Lucas Pereira <lucasgonp@gmail.com>
2022-01-10 17:14:54 -0300

    add end-to-end test to guarantee the same email does not register twice

--- UseCasesIntegrationTests/AddAccountIntegrationTests.swift
+++ UseCasesIntegrationTests/AddAccountIntegrationTests.swift
@@ -8,7 +8,7 @@ class UseCasesIntegrationTests: XCTestCase {
         let alamofireAdapter = AlamofireAdapter()
         let url = URL(string: "https://fordevs.herokuapp.com/api/signup")!
         let sut = RemoteAddAccount(url: url, httpClient: alamofireAdapter)
-        let addAccountModel = AddAccountModel(name: "Lucas Pereira", email: "LucasPereira001@mail.com", password: "any_password", passwordConfirmation: "any_password")
+        let addAccountModel = AddAccountModel(name: "Lucas Pereira", email: "\(UUID().uuidString)@mail.com", password: "any_password", passwordConfirmation: "any_password")
         let exp = expectation(description: "waiting")
         sut.add(addAccountModel: addAccountModel) { result in
             switch result {
@@ -19,5 +19,16 @@ class UseCasesIntegrationTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 5)
+        let exp2 = expectation(description: "waiting")
+        sut.add(addAccountModel: addAccountModel) { result in
+            switch result {
+            case .failure(let error) where error == .emailInUse:
+                XCTAssertNotNil(error)
+            default:
+                XCTFail("Expect success got \(result) instead")
+            }
+            exp2.fulfill()
+        }
+        wait(for: [exp2], timeout: 5)
     }
 }
