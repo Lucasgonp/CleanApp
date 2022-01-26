6c09828fc8745983ab8446a53058384df0f835e9
Lucas Pereira <lucasgonp@gmail.com>
2022-01-26 13:46:20 -0300

    change navigationController init

--- Main/SceneDelegate.swift
+++ Main/SceneDelegate.swift
@@ -7,10 +7,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        let nav = NavigationController()
         let httpClient = makeAlamofireAdapter()
         let authentication = makeRemoteAuthentication(httpClient: httpClient)
         let loginController = makeLoginController(authentication: authentication)
-        let nav = NavigationController(rootViewController: loginController)
+        nav.setRootViewController(loginController)
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
     }
--- UI/Components/NavigationController.swift
+++ UI/Components/NavigationController.swift
@@ -2,15 +2,24 @@ import Foundation
 import UIKit
 
 public final class NavigationController: UINavigationController {
-    public override init(rootViewController: UIViewController) {
-        super.init(rootViewController: rootViewController)
-        setup()
-    }
-    
-    public required init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    public convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
+    
+    public override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     private func setup() {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
@@ -21,8 +30,12 @@ public final class NavigationController: UINavigationController {
     }
 }
 
-extension NavigationController {
-    public override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
+public extension NavigationController {
+    func setRootViewController(_ viewcontroller: UIViewController) {
+        setViewControllers(viewControllers, animated: true)
+    }
+    
+    func pushViewController(_ viewcontroller: UIViewController) {
+        pushViewController(viewcontroller, animated: true)
     }
 }
