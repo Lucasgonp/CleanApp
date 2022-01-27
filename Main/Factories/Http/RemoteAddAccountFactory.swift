ecc41081c9182c934bd6022514924fd871dc2617
Lucas Pereira <lucasgonp@gmail.com>
2022-01-26 21:28:57 -0300

    update signup endpoint path

--- Main/Factories/Http/RemoteAddAccountFactory.swift
+++ Main/Factories/Http/RemoteAddAccountFactory.swift
@@ -7,6 +7,6 @@ func makeRemoteAddAccount() -> AddAccount {
 }
 
 func makeRemoteAddAccountWith(httpClient: HttpPostClient) -> AddAccount {
-    let remoteAddAccount = RemoteAddAccount(url: makeApiUrl(path: "register"), httpClient: httpClient)
+    let remoteAddAccount = RemoteAddAccount(url: makeApiUrl(path: "signup"), httpClient: httpClient)
     return MainQueueDispatchDecorator(remoteAddAccount)
 }
